64e6541e66ffcaad9d9440c769cfa533c775670c
Rafael Rosa <114161023+rafaeelrosa568@users.noreply.github.com>
2026-06-19 18:00:07 -0300

    Refactor Firebase configuration and imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,39 +1,18 @@
-import { initializeApp }
-from "firebase/app";
-
-import { getAuth }
-from "firebase/auth";
-
-import { getFirestore }
-from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  apiKey:
-    import.meta.env.VITE_API_KEY,
-
-  authDomain:
-    import.meta.env.VITE_AUTH_DOMAIN,
-
-  projectId:
-    import.meta.env.VITE_PROJECT_ID,
-
-  storageBucket:
-    import.meta.env.VITE_STORAGE_BUCKET,
-
-  messagingSenderId:
-    import.meta.env
-      .VITE_MESSAGING_SENDER_ID,
-
-  appId:
-    import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyBfzjtAix-QyafEWPUowKe8_puQ8TNetEw",
+  authDomain: "sistema-manutencao-c027e.firebaseapp.com",
+  projectId: "sistema-manutencao-c027e",
+  storageBucket: "sistema-manutencao-c027e.firebasestorage.app",
+  messagingSenderId: "493600150897",
+  appId: "1:493600150897:web:443fa37a37373da37fd2d0"
 };
 
-const app =
-  initializeApp(firebaseConfig);
 
-export const auth =
-  getAuth(app);
+const app = initializeApp(firebaseConfig);
 
-export const db =
-  getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
