8538d863a356cb49f8f0c7b987a78ca2f36147c3
Rafael Rosa <114161023+rafaeelrosa568@users.noreply.github.com>
2026-06-20 10:09:04 -0300

    Atualizar o routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,17 +9,14 @@ import Dashboard from "./pages/Dashboard";
 import Usuarios from "./pages/Usuarios";
 import Veiculos from "./pages/Veiculos";
 import Manutencoes from "./pages/Manutencoes";
-import AlterarSenha
-from "./pages/AlterarSenha";
+import ManutencoesEncerradas from "./pages/ManutencoesEncerradas";
+import AlterarSenha from "./pages/AlterarSenha";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function AppRoutes() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
 
         <Route
@@ -46,31 +43,38 @@ export default function AppRoutes() {
         />
 
         <Route
-  path="/veiculos"
-  element={
-    <ProtectedRoute>
-      <Veiculos />
-    </ProtectedRoute>
-  }
-/>
+          path="/veiculos"
+          element={
+            <ProtectedRoute>
+              <Veiculos />
+            </ProtectedRoute>
+          }
+        />
 
-  <Route
-  path="/manutencoes"
-  element={
-    <ProtectedRoute>
-      <Manutencoes />
-    </ProtectedRoute>
-  }
-/>
+        <Route
+          path="/manutencoes"
+          element={
+            <ProtectedRoute>
+              <Manutencoes />
+            </ProtectedRoute>
+          }
+        />
 
-<Route
-  path="/alterar-senha"
-  element={<AlterarSenha />}
-/>
+        <Route
+          path="/manutencoesEncerradas"
+          element={
+            <ProtectedRoute>
+              <ManutencoesEncerradas />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/alterar-senha"
+          element={<AlterarSenha />}
+        />
 
       </Routes>
-
     </BrowserRouter>
-
   );
 }
